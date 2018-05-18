e499b624dc0294b6040d531e97a00c0af64ebd38
meres <kissslorinc@gmail.com>
2018-05-18 03:47:20 -0700

    added interaction test

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,36 +8,39 @@ import org.junit.Test;
 
 public class GT4500Test {
 
-  private TorpedoStore mockPrimary, mockSecondary;
+  private TorpedoStore mockStore;
   private GT4500 ship;
 
   @Before
   public void init(){
-    mockPrimary = mock(TorpedoStore.class);
-    mockSecondary = mock(TorpedoStore.class);
-    this.ship = new GT4500(mockPrimary,mockSecondary);
+    mockStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockStore,mockStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockStore, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(mockStore, times(2)).fire(1);
   }
 
 }
